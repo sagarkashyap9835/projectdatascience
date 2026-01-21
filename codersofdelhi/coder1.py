1b79e4e6da0449ba638b305e144f9042e824d9ed
sagarkashyap9835 <vidyasagar983531@gmail.com>
2026-01-21 14:59:42 +0530

    first project completed

--- codersofdelhi/coder1.py
+++ codersofdelhi/coder1.py
@@ -1,4 +1,5 @@
 import json
+# lets write a function to load the data
 def load_data(filename):
     with open(filename,"r") as f:
         data=json.load(f)
@@ -6,3 +7,16 @@ def load_data(filename):
 data=load_data("codersofdelhi/data1.json")
 print(data)
 print(type(data))
+
+# write a function to display users and their connections
+
+def display_users(data):
+    print("Users and their connections\n")
+    for user in data['users']:
+        print(f"ID:{user['id']} - {user['name']} is friends with: {user['friends']} and liked pages are {user['liked_pages']} ")
+    print("pages information")
+    for page in data['pages']:
+        print(f"{page['id']}: {page['name']} ")
+print(display_users(data))     
+
+   
